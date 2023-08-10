a8b0daa8a8e91c4ea9384a891eaa0da7a216ca98
anders1901 <anderssonroberto@hotmail.fr>
2023-08-10 13:57:29 +0200

    Update PQCtest_filter.c

--- additional_materials/c_materials/PQCtest_filter.c
+++ additional_materials/c_materials/PQCtest_filter.c
@@ -1,8 +1,5 @@
 //
-//  PQCgenKAT_sign.c
-//
-//  Created by Bassham, Lawrence E (Fed) on 8/29/17.
-//  Copyright © 2017 Bassham, Lawrence E (Fed). All rights reserved.
+//  PQCtest_filter.c
 //
 #include <stddef.h>
 #include <stdint.h>
@@ -15,7 +12,6 @@
 #define KAT_SUCCESS          0
 #define KAT_FILE_OPEN_ERROR -1
 #define KAT_DATA_ERROR      -3
-#define KAT_CRYPTO_FAILURE  -4
 
 int	FindMarker(FILE *infile, const char *marker);
 int	ReadHex(FILE *infile, unsigned char *a, int Length, char *str);
@@ -27,25 +23,14 @@ main()
 {
     char                fn_req[35], fn_rsp[34];
     FILE                *fp_req, *fp_rsp;
-    uint8_t             seed[48];
     uint8_t             msg[3300];
-    uint8_t             m[32];
     int8_t              sm[3300];
     uint8_t             entropy_input[48];
-    // uint8_t             *m, *m1;
-    size_t              mlen, smlen, mlen1;
-    int                 count;
-    int                 w0, cpt_indices;
-    int                 done;
+    size_t              smlen;
     uint8_t             pk[CRYPTO_PUBLICKEYBYTES], sk[CRYPTO_SECRETKEYBYTES];
-    int                 ret_val;
-    int                 cpt = 0;
     int                 NB = 1000000;
-    int                 targeted_value = 0;
-    uint8_t             hw_w0;
     int                 number = 0;
     long                i = 0;
-    long long int       global_counter = 0;
     uint64_t            w0_to_0_detected_filter = 0, w0_to_0_total = 0, values_detected_filter = 0;
     long long int       total_w0_to_0_detected_filter = 0, total_w0_to_0_total = 0, total_values_detected_filter = 0;
 
@@ -69,7 +54,6 @@ main()
         return KAT_FILE_OPEN_ERROR;
     }
 
-    done = 0;
     if ( !ReadHex(fp_req, pk, (int)CRYPTO_PUBLICKEYBYTES, "pk = ") ) {
         printf("ERROR: unable to read 'pk' from <%s>\n", fn_req);
         return KAT_DATA_ERROR;
@@ -96,7 +80,6 @@ main()
         i++;
 
         if (i%25000 == 0){
-            // printf("%d/%d\r", i, NB);
             printf("%d/%ld\r", number, i);
             fflush(stdout);
         }
